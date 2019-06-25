9b71eff70ab1d6f6d720e5addd6ee0baa07dcbb3
PetejStone <petejstoney@gmail.com>
2019-06-25 09:50:13 -0700

    deploying to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+const dbConnection = process.env.DATABASE_URL
 module.exports = {
 
   development: {
@@ -48,19 +48,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: dbConnection,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   }
 
 };
